b808dabfbda3d327a8dff277d48818bad5f9cfa3
theesan-amg <theesan270@outlook.com>
2021-08-05 17:22:23 +0200

    Create html/css/javascript.modified backend

--- api/server.js
+++ api/server.js
@@ -1,16 +1,22 @@
+const countries = require("./dataCountries.json");
 const express = require("express");
 const app = express();
-const port = 8000;
- const {countriesData} = require('./countriesData.json');
+const port = 8100;
 
-// app.get('/all', (req,res) => {
-// console.log("Salut");
-// res.json(countriesData)
-// });
+app.listen(port, () => {
+    console.log(port);
+    
+});
 
-app.listen(port, () =>{
-    console.log('server started on port:' + port);
-})
+// parcourir les pays dans json
+app.get("/all", (req, res) => {
+  let countriesNames = [];
+  for (let i = 0; i < countries.length; i++) {
+    countriesNames.push(countries[i].name);
+  }
+  res.send(countriesNames.join(", "));
+});
+// console.log(countries.name);
 
 
 
